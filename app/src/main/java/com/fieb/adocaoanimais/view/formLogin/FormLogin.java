ef17d2b55c3468b11ce95ea9eae0013e08850c7b
Sousa-Edson <edson3711.es@gmail.com>
2023-04-18 22:09:59 -0300

    criação de tela home inicio

--- app/src/main/java/com/fieb/adocaoanimais/view/formLogin/FormLogin.java
+++ app/src/main/java/com/fieb/adocaoanimais/view/formLogin/FormLogin.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import com.fieb.adocaoanimais.R;
 import com.fieb.adocaoanimais.databinding.ActivityFormLoginBinding;
 import com.fieb.adocaoanimais.view.formCadastro.FormCadastro;
+import com.fieb.adocaoanimais.view.home.Home;
 import com.fieb.adocaoanimais.view.telaPrincipal.TelaPrincipal;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
@@ -51,7 +52,8 @@ public class FormLogin extends AppCompatActivity {
     }
 
     private void navegarTelaPrincipal() {
-        Intent intent = new Intent(this, TelaPrincipal.class);
+//        Intent intent = new Intent(this, TelaPrincipal.class);
+        Intent intent = new Intent(this, Home.class);
         startActivity(intent);
         finish();
     }
